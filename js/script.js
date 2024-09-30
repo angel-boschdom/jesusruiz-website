83f66de3f61483dc98aaf737cc8d3d68ceb83d5a
Angel Gonzalez <angel.gon.llacer@gmail.com>
2024-09-30 17:06:58 +0100

    responsive burger menu

--- js/script.js
+++ js/script.js
@@ -67,3 +67,26 @@ function fadeInOnScroll() {
 
 window.addEventListener('scroll', fadeInOnScroll);
 window.addEventListener('load', fadeInOnScroll);
+
+// Toggle Navigation Menu for Mobile
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+const body = document.body;
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active');
+    navLinks.classList.toggle('open');
+    body.classList.toggle('menu-open');
+});
+
+// Close the menu when a navigation link is clicked (optional)
+const navItems = navLinks.querySelectorAll('a');
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        if (navLinks.classList.contains('open')) {
+            menuIcon.classList.remove('active');
+            navLinks.classList.remove('open');
+            body.classList.remove('menu-open');
+        }
+    });
+});
